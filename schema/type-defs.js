2f93ca36075c00f216d7efdb98c7b0b192a92f23
KrishnaKEA <kris47f9@stud.kea.dk>
2023-01-03 18:58:52 +0100

    crud working

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -5,11 +5,55 @@ const typeDefs = gql`
         name: String!
         username: String!
         age: Int!
-        nationality: String!
+        nationality: Nationality!
+        friends: [User]
+        favoriteMovies:[Movie]
+    }
+
+    type Movie {
+        id: ID!
+        name: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!
     }
 
     type Query {
     users: [User!]!
+    user(id: ID!) : User!
+    movies:[Movie!]!
+    movie(name: String!): Movie!
+    }
+
+    input CreateUserInput{
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality = "INDIA"
+        
+    }
+    input UpdateUsernameInput{
+        id:ID!
+        newUsername: String!
+        
+    }
+
+
+
+    type Mutation {
+        createUser(input: CreateUserInput!):User!
+        updateUsername(input:UpdateUsernameInput!):User
+        deleteUser(id: ID!): User
+    }
+
+
+
+
+    enum Nationality {
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
     }
 
 `;
